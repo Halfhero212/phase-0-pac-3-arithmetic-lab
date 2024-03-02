c23788f4aa823c4d004e655c33cfe2e1101467cb
Nooralhussein <lighthero212@gmail.com>
2024-03-02 20:31:53 +0300

    Done

---
+++ index.js
@@ -0,0 +1,48 @@
+// Introduction to Functions Lab solutions
+
+function introduction(name) {
+    return `Hi, my name is ${name}.`;
+  }
+  
+  function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+  }
+  
+  function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+  }
+  
+  // Practice Doing Math with JavaScript solutions
+  
+  function add(a, b) {
+    return a + b;
+  }
+  
+  function subtract(a, b) {
+    return a - b;
+  }
+  
+  function multiply(a, b) {
+    return a * b;
+  }
+  
+  function divide(a, b) {
+    return a / b;
+  }
+  
+  function increment(n) {
+    return ++n;
+  }
+  
+  function decrement(n) {
+    return --n;
+  }
+  
+  function makeInt(string) {
+    return parseInt(string, 10);
+  }
+  
+  function preserveDecimal(string) {
+    return parseFloat(string);
+  }
+  
